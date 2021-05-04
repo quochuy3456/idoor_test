80dfa58d2ae15713a9e575b81f43e7c2d9f74c84
huy.do1 <quochuy3456@gmail.com>
2021-05-04 08:44:33 +0700

    addddd

--- server_tst.py
+++ server_tst.py
@@ -1,7 +1,9 @@
 import urllib.parse
 from flask import request, redirect, jsonify, render_template
 from flask_wtf.csrf import CSRFProtect
+from flask_wtf import FlaskForm
 from flask_cors import CORS, cross_origin
+from wtforms import StringField
 import time
 import base64
 import sys
@@ -12,10 +14,14 @@ from flask import Flask
 app = Flask(__name__,  template_folder='templates')
 # app.config['WTF_CSRF_FIELD_NAME'] = 'csrf_token'
 csrf = CSRFProtect(app)
-token = csrf._get_csrf_token()
+# token = csrf._get_csrf_token()
 app.config['SECRET_KEY'] = "Hgdjihfoidhfoisvjhosvjnovndj2423kj4j234"
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app, resources={r"/cgi-bin/basic": {"origins": "*"}})
+
+
+class tst_form(FlaskForm):
+    token = StringField("Token: ")
 
 
 @app.route('/cgi-bin/basic', methods=['POST', ])
@@ -27,9 +33,10 @@ def cgi_bin_basic():
         return jsonify({"result": "Success", "token": token})
 
 
-# @app.errorhandler(CSRFError)
-# def handle_csrf_error(e):
-#     return render_template('csrf_error.html', reason=e.description), 400
+@app.route('/cgi-bin/test')
+def handle_csrf_error():
+    form = tst_form()
+    return render_template('form2.html', form=form)
 
 
 if __name__ == '__main__':
